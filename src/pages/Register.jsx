f414a6fa16237c2d684a115accba6214b4a57fd7
Umar Qazi <umarqazii983@gmail.com>
2024-09-12 15:38:42 +0500

    login/ signup page made simpler, manifest.json updated

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -41,33 +41,12 @@ function Register() {
       });
 
   };
-  useEffect(() => {
-    var typedPhrase = new Typed('.typedPhrase', {
-      strings: ['Guessing Game'],
-      typeSpeed: 80,
-      showCursor: true,
-    });
-
-    return () => {
-      typedPhrase.destroy();
-    };
-  }, []);
 
   return (
     <div className='App'>
       <MDBContainer fluid>
       <MDBRow className='d-flex justify-content-center align-items-center h-100'>
-        <MDBCol md='6' className='text-center text-md-start d-flex flex-column justify-content-center'>
-
-          <img src={logo} alt="logo" className="logo" style={{ maxWidth: '350px', height: 'auto', display: 'block', margin: '0 auto', marginTop: "80px" }} />
-
-          <h1 className="my-5 display-3 fw-bold ls-tight px-3 fancy-text gradient-text" style={{ display: 'block', margin: '0 auto' }}>
-            <span id="element" className="typedPhrase" style={{ color: '#72c4bd', fontFamily: 'Audiowide, sans-serif', fontSize: '50px' }}></span>
-          </h1>
-
-
-
-        </MDBCol>
+        
         <MDBCol col='12'>
           <MDBCard className='bg-dark text-white my-5 mx-auto' style={{ borderRadius: '1rem', maxWidth: '400px' }}>
             <MDBCardBody className='p-5 d-flex flex-column align-items-center mx-auto w-100'>
